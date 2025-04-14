6006e45eb2c67e0ac7874723e24542fb1bd7b748
Sandro Gomez <sandrodevx@gmail.com>
2025-04-14 19:58:47 +0000

    GeneradorCartas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
-import "bootstrap";
-import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  generateRandomCard();
+  
+  document.querySelector('#newCardBtn').addEventListener('click', generateRandomCard);
+  
+  // Opcional: Temporizador para cambiar carta cada 10 segundos
+  setInterval(generateRandomCard, 10000);
 };
+
+function generateRandomCard() {
+  const card = document.querySelector('#card');
+  // Palos
+  const suits = ['heart', 'diamond', 'spade', 'club'];
+  const randomSuit = suits[Math.floor(Math.random() * suits.length)]; 
+  // Valores
+  const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+  const randomValue = values[Math.floor(Math.random() * values.length)]; 
+
+  // Remover Clases anteriores y agregar nuevas 
+  card.className = 'card ' + randomSuit;
+  card.innerHTML = `<div style= "font-size: 80px; text-align: center; margin: auto;">${randomValue}</div>`;
+
+}
